a9396fdd79c971ddb92ace83c0780689ee9db162
Kyle Couch <kacouc02@louisville.edu>
2014-08-07 14:52:46 -0400

    Added Addition Method
    The user can now enter addition and will be prompted to enter two
    numbers. On the completion of entering the numbers the result will be
    displayed. If the user enters an operation other than addition they are
    notified that the operation is invalid.

--- KCouchJavaCalc/src/kcouchjavacalc/KCouchJavaCalc.java
+++ KCouchJavaCalc/src/kcouchjavacalc/KCouchJavaCalc.java
@@ -16,13 +16,48 @@ public class KCouchJavaCalc {
     /**
      * @param args the command line arguments
      */
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
+        double result = 0;
+        boolean flag = false;
         System.out.println("Java Calculator\nWe can do:\nAddiiton\nSubtraction");
         System.out.println("What operation you like to do?");
-        Scanner scan = new Scanner(System.in);
+        // Scanner scan = new Scanner(System.in);
         String operation = scan.nextLine();
-        System.out.println(operation);
+        //System.out.println(operation);
+        if(operation.equals("addition"))
+        {
+            result = Addition();
+        }
+        else
+        {
+            flag = true;
+        }
+        if(flag)
+        {
+            System.out.println("Invalid Operation");
+        }
+        else
+        {
+            System.out.println("The result is: "+result); 
+        }
         
+    }
+    
+    //Addition
+    public static double Addition()
+    {
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+        
+        System.out.println("You Chose Addition!\n"
+                + "Enter the first number");
+        num1 = scan.nextInt();
+        System.out.println("Enter the second number");
+        num2 = scan.nextInt();
+        result = num1+num2;
+        return result;
     }
 
 }
